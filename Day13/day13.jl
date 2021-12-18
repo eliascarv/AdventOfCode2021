fe383a569826e2edf9c1901d92d8dba1fc8694fb
Elias Carvalho <eliascarvdev@gmail.com>
2021-12-18 09:54:09 -0300

    Day 13

--- Day13/day13.jl
+++ Day13/day13.jl
@@ -1,5 +1,6 @@
 using SparseArrays
 
+## Data -------------------------------------------------------------
 lines = readlines(joinpath(@__DIR__, "input.txt"))
 
 const CI = CartesianIndex
@@ -17,12 +18,40 @@ function parsedata(lines)
         var, val = split(lines[i][12:end], "=")
         folds[i-n] = Symbol(var) => parse(Int, val)
     end
-    y = maximum(i -> i[1], idxs)
-    x = maximum(i -> i[2], idxs)
-    M = sparse(zeros(Int, y, x))
-    M[idxs] .= 1
+    ymax = maximum(i -> i[1], idxs)
+    xmax = maximum(i -> i[2], idxs)
+    data = sparse(zeros(Int, ymax, xmax))
+    data[idxs] .= 1
+    return data, folds
+end
+
+data, folds = parsedata(lines)
+
+## Part 1 -----------------------------------------------------------
+hrot(M) = hcat([reverse(col) for col in eachcol(M)]...)
+vrot(M) = permutedims(hcat([reverse(row) for row in eachrow(M)]...))
+
+function foldpaper(data, fold::Pair{Symbol, Int})
+    v = fold[1]
+    n = fold[2] + 1
+    if v == :y
+        M = data[1:n-1, :] + hrot(data[n+1:end, :])
+    elseif v == :x
+        M = data[:, 1:n-1] + vrot(data[:, n+1:end])
+    end
     return M
 end
 
-lines = readlines(joinpath(@__DIR__, "demo.txt"))
-M = parsedata(lines)
+# Solution: 653
+count(>(0), foldpaper(data, folds[1]))
+
+## Part 2 -----------------------------------------------------------
+function foldpaper(data, folds::Vector{Pair{Symbol, Int}})
+    for fold in folds
+        data = foldpaper(data, fold)
+    end
+    return data
+end
+
+# Solution: LKREBPRK
+foldpaper(data, folds)
