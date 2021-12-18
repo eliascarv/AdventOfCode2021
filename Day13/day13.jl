399616a2676173dab8931b2b526b2f9e2ada820c
Elias Carvalho <eliascarvdev@gmail.com>
2021-12-18 11:50:21 -0300

    Update Day 13

--- Day13/day13.jl
+++ Day13/day13.jl
@@ -7,14 +7,14 @@ function parsedata(lines)
     l = length(lines)
     d = findfirst(isempty, lines)
     idxs = Vector{NTuple{2, Int}}(undef, d - 1)
-    folds = Vector{Pair{Symbol, Int}}(undef, l - d)
+    folds = Vector{Tuple{Symbol, Int}}(undef, l - d)
     for i in 1:d-1
         x, y = parse.(Int, split(lines[i], ','))
         idxs[i] = (y + 1, x + 1)
     end
     for i in d+1:l
         v, n = split(lines[i][12:end], '=')
-        folds[i-d] = Symbol(v) => parse(Int, n) + 1
+        folds[i-d] = (Symbol(v), parse(Int, n) + 1)
     end
     data = sparse(first.(idxs), last.(idxs), 1)
     return data, folds
@@ -23,7 +23,7 @@ end
 data, folds = parsedata(lines)
 
 ## Part 1 -----------------------------------------------------------
-function foldpaper(data, fold::Pair{Symbol, Int})
+function foldpaper(data, fold::Tuple{Symbol, Int})
     v, n = fold
     if v == :y
         data = data[1:n-1, :] + data[end:-1:n+1, :]
@@ -37,7 +37,7 @@ end
 count(>(0), foldpaper(data, folds[1]))
 
 ## Part 2 -----------------------------------------------------------
-function foldpaper(data, folds::Vector{Pair{Symbol, Int}})
+function foldpaper(data, folds::Vector{Tuple{Symbol, Int}})
     for fold in folds
         data = foldpaper(data, fold)
     end
