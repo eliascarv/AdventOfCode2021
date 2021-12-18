4bf1215fb42ccb7253fde4a7b0471576f3b7e8ee
Elias Carvalho <eliascarvdev@gmail.com>
2021-12-18 11:27:31 -0300

    Update Day 13

--- Day13/day13.jl
+++ Day13/day13.jl
@@ -3,25 +3,20 @@ using SparseArrays
 ## Data -------------------------------------------------------------
 lines = readlines(joinpath(@__DIR__, "input.txt"))
 
-const CI = CartesianIndex
-
 function parsedata(lines)
     l = length(lines)
     d = findfirst(isempty, lines)
-    idxs = Vector{CI{2}}(undef, d - 1)
+    idxs = Vector{NTuple{2, Int}}(undef, d - 1)
     folds = Vector{Pair{Symbol, Int}}(undef, l - d)
     for i in 1:d-1
         x, y = parse.(Int, split(lines[i], ','))
-        idxs[i] = CI(y + 1, x + 1)
+        idxs[i] = (y + 1, x + 1)
     end
     for i in d+1:l
         v, n = split(lines[i][12:end], '=')
         folds[i-d] = Symbol(v) => parse(Int, n) + 1
     end
-    ymax = maximum(i -> i[1], idxs)
-    xmax = maximum(i -> i[2], idxs)
-    data = sparse(zeros(Int, ymax, xmax))
-    data[idxs] .= 1
+    data = sparse(first.(idxs), last.(idxs), 1)
     return data, folds
 end
 
