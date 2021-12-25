30e6cb51c1b48a8be9806a4bd4315a5eaf4c853e
Elias Carvalho <eliascarvdev@gmail.com>
2021-12-24 21:01:26 -0300

    Update Day 14

--- Day14/day14.jl
+++ Day14/day14.jl
@@ -5,38 +5,29 @@ polymer = lines[1]
 rulers = Dict(p => r for (p, r) in split.(lines[3:end], " -> "))
 
 ## Functions ------------------------------------------------------------------
-function simulate(polymer, rulers, steps)
-    sim = Dict(p => count(p, polymer, overlap=true) for p in keys(rulers))
+function countchars(polymer, rulers, steps)
+    cntpairs = Dict(p => count(p, polymer, overlap=true) for p in keys(rulers))
+    cntchars = Dict(string(c) => count(c, polymer) for c in unique(polymer))
     for _ in 1:steps
-        temp = filter(p -> last(p) > 0, sim)
+        temp = filter(p -> last(p) > 0, cntpairs)
         for p in keys(temp)
-            sim[p] -= temp[p]
-            sim[p[1]*rulers[p]] += temp[p]
-            sim[rulers[p]*p[2]] += temp[p]
+            cntpairs[p] -= temp[p]
+            cntpairs[p[1]*rulers[p]] += temp[p]
+            cntpairs[rulers[p]*p[2]] += temp[p]
+            cntchars[rulers[p]] += temp[p]
         end
     end
-    return sim
-end
-
-function countchars(polymer, sim)
-    counts = Dict{Char, Int}()
-    for char in 'A':'Z'
-        counts[char] = sum(values(filter(p -> endswith(first(p), char), sim)))
-    end
-    counts[polymer[1]] += 1
-    return filter(p -> last(p) > 0, counts)
+    return cntchars
 end
 
 ## Part 1 ---------------------------------------------------------------------
-sim1 = simulate(polymer, rulers, 10)
 
 # Solution: 2194
-counts = values(countchars(polymer, sim1))
+counts = values(countchars(polymer, rulers, 10))
 maximum(counts) - minimum(counts)
 
 ## Part 2 ---------------------------------------------------------------------
-sim2 = simulate(polymer, rulers, 40)
 
 # Solution: 2360298895777
-counts = values(countchars(polymer, sim2))
+counts = values(countchars(polymer, rulers, 40))
 maximum(counts) - minimum(counts)
